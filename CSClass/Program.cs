80fc6b8a3c518c95265b996418288b7eb3ea3486
Hyuna Yu <s2012@e-mirim.hs.kr>
2022-05-20 09:53:57 +0900

    #2 Math 클래스 메서드들

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -35,6 +35,15 @@ namespace CSClass
             {
                 Console.WriteLine("Count : " + list.Count + "\titem : " + item);
             }
+
+            //Math 클래스
+            Console.WriteLine(Math.Abs(-52273));
+            Console.WriteLine(Math.Ceiling(52.273));
+            Console.WriteLine(Math.Floor(52.273));
+            Console.WriteLine(Math.Max(52, 273));
+            Console.WriteLine(Math.Min(52, 273));
+            Console.WriteLine(Math.Round(52.273));
+            Console.WriteLine(Math.PI);
         }
 
     }
